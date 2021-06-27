5dd3df36192edcb27338e3b0b94c4b9f19e1e1a5
mehmetsahan58 <67297513+mehmetsahan58@users.noreply.github.com>
2021-06-27 17:32:54 -0500

    Python Practices

---
+++ venv/variables.py
@@ -0,0 +1,12 @@
+
+name = "Robert Cruz"
+
+type_of_car = "SUV"
+
+school = "Hawk Elemenntary School"
+
+print (name + " " + school + ".")
+
+print (name + " work at " + school +".")
+
+print ("{} works at {}.".format(name, school))
